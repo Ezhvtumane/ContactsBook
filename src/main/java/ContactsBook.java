747a5563984ce74fd7fb8beb8a07caf9776ca0ad
Ezh_X_U <gogaopel@gmail.com>
2016-05-22 14:21:48 +0300

    Initial commit

---
+++ src/main/java/ContactsBook.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Created by ubuntu on 21.05.16.
+ */
+public class ContactsBook {
+
+    ArrayList<Person> persons;
+
+
+
+}
---
+++ src/main/java/Person.java
@@ -0,0 +1,10 @@
+/**
+ * Created by ubuntu on 21.05.16.
+ */
+public class Person {
+
+    String name;
+    String phoneNumber;
+    String email;
+
+}
